420d80cc1c7f0fa836dcd4adabfe311d1d63927d
Swathi V Chavan <155572512+swathivc@users.noreply.github.com>
2024-06-29 21:23:14 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000",
+    },
+  },
+  plugins: [react(), nodePolyfills()],
+});
